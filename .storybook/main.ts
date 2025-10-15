c2404eac2e63afee5e8427549f82dc0090381623
kapernikxd <kapernikxd@gmail.com>
2025-10-15 21:39:28 +0200

    ????

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,6 @@
 const config = {
   stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-ondevice-controls',
-    '@storybook/addon-ondevice-actions',
-  ],
+  addons: ["@storybook/addon-docs", "@chromatic-com/storybook"],
   framework: {
     name: '@storybook/react-native-web-vite',
     options: {},

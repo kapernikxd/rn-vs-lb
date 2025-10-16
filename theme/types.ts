a7a2cc84733ba77e374f58e907efff6fa9320628
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-10-16 18:41:37 +0200

    Fix ThemeType re-export as type

--- theme/types.ts
+++ theme/types.ts
@@ -1,4 +1,4 @@
 export type { CommonStylesType } from './styles/style';
 export type { GlobalStyleSheetType, TypographytType } from './styles/styleSheet';
 export type { FormStylesType } from './styles/commonFormStyles';
-export { ThemeType, SizesType, FontsType } from "./theme";
+export type { ThemeType, SizesType, FontsType } from "./theme";

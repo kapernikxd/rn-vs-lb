46a27c330f76ba33eb817be8c2d3f37c1fa685cb
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-03 10:52:36 +0100

    Shim vector icons for storybook web build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import { mergeConfig } from "vite";
 import { StorybookConfig } from "@storybook/react-native-web-vite";
 
 const main: StorybookConfig = {
@@ -11,6 +13,25 @@ const main: StorybookConfig = {
   framework: {
     name: "@storybook/react-native-web-vite",
     options: {},
+  },
+
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "react-native": "react-native-web",
+          "react-native$": "react-native-web",
+          "react-native-vector-icons":
+            "@expo/vector-icons/build/vendor/react-native-vector-icons",
+          "react-native-vector-icons/lib/create-icon-set": path.resolve(
+            __dirname,
+            "./shims/create-icon-set.web"
+          ),
+          "@expo/vector-icons/build/vendor/react-native-vector-icons/lib/create-icon-set":
+            path.resolve(__dirname, "./shims/create-icon-set.web"),
+        },
+      },
+    });
   },
 };
 

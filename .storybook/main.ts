f35711e178aa609b0cbdc53561fe8fa4ff7b207d
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-03 10:04:43 +0100

    Fix Storybook web build aliases and exports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import { mergeConfig } from "vite";
 import { StorybookConfig } from "@storybook/react-native-web-vite";
 
 const main: StorybookConfig = {
@@ -11,6 +12,17 @@ const main: StorybookConfig = {
   framework: {
     name: "@storybook/react-native-web-vite",
     options: {},
+  },
+
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "react-native": "react-native-web",
+          "react-native$": "react-native-web",
+        },
+      },
+    });
   },
 };
 
